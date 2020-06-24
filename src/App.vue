3c0557424b901f57c1abee957393ca36e43bbbaf
Jeongmu.Park <jeongmupark@gmail.com>
2020-06-24 15:16:29 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <container></container>
+  </div>
+</template>
+
+<script>
+import Container from "@/components/Container.vue";
+
+export default {
+  components: {
+    Container
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Container.vue
@@ -0,0 +1,36 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sDate: new Date(),
+      eDate: new Date(),
+
+      arr: [1, 2, 3],
+      num: 4
+    };
+  },
+
+  mounted() {
+    this.sDate.setMonth(this.eDate.getMonth() - 3);
+
+    // console.log(this.sDate);
+    // console.log(this.eDate);
+
+    console.log(this.arr.every(this.checkVal));
+  },
+
+  methods: {
+    checkVal(val) {
+      return val !== this.num;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
+
