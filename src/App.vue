bcee3960efef7b550b18602544d5fbc9e93c4321
Jeongmu.Park <jeongmupark@gmail.com>
2020-06-26 10:15:21 +0900

    [테스트중] date, every 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <container></container>
+    <date></date>
   </div>
 </template>
 
 <script>
 import Container from "@/components/Container.vue";
+import Date from "@/components/Date.vue";
 
 export default {
   components: {
-    Container
+    Container,
+    Date
   }
 };
 </script>
---
+++ src/components/Date.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sDate: new Date(),
+      eDate: new Date()
+    };
+  },
+
+  mounted() {
+    this.sDate.setMonth(this.eDate.getMonth() - 3);
+  },
+
+  methods: {}
+};
+</script>
+
+<style>
+</style>
