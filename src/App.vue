dd3bdd4dcd965c2b7d53b2aa19127ef4731fba5a
Jeongmu.Park <jeongmupark@gmail.com>
2020-08-16 01:35:38 +0900

    Date 객체 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
     <container></container>
-    <date></date>
   </div>
 </template>
 
 <script>
 import Container from "@/components/Container.vue";
-import Date from "@/components/Date.vue";
 
 export default {
   components: {
